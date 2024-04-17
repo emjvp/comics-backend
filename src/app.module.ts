15f0ab5b99570d766076f77e4f5225df4630f2b0
Emilio Vargas <emjvp37@gmail.com>
2024-04-16 22:23:52 -0500

    instalando HttpModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ComicsController } from './comics/controllers/comics.controller';
+import { ComicsService } from './comics/services/comics/comics.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HttpModule],
+  controllers: [AppController, ComicsController],
+  providers: [AppService, ComicsService],
 })
 export class AppModule {}
